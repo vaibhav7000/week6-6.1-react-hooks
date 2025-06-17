692b1c92bfe8a2ddb0671a7de076705a1691cb17
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-17 21:53:46 +0530

    updaing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./Components/Header.jsx";
 import React, { Fragment, useState } from "react";
 import HeaderWithButton from "./Components/HeaderWithButton.jsx";
+import TodoInputMemo from "./Components/TodoInput.jsx";
+import { AllTodosMemo } from "./Components/AllTodos.jsx";
 
 
 function App () {
@@ -19,6 +21,7 @@ function App () {
 
       <Header  title={"Hello JS"}/>
 
+      <HeaderMemo title={"Hellos JSSS"} />
 
       <button style={{
         margin: 20
@@ -41,3 +44,69 @@ export default App;
 // Only the custom maded Components get re-rendered
 
 // We want less components to get re-render, lesser the components re-render / refresh more optimal website it will be
+
+// TO make the optimal React app (less re-renders), maintain the state variable at the lowest level parent / or use React-memo -> This Hook will not upadte the children if the props pass to them does not changed
+
+
+const HeaderMemo = React.memo(function({title}) {
+  return (
+    <div>
+      {title}
+    </div>
+  )
+})
+
+// React memo lets to skip re-rendering when the props passed to it are unchanged => if the parent has different state variables and different state-variables are passed to different components, => change in the state-variable will cause the Parent to re-render ( call the function again ) + all the Child components that uses that state-variable will be gets changed
+
+// The state-variables should always gets passed from Parent to Child not from Child to Parent
+
+
+
+function TodoApp() {
+  // This is the parent that will have all todos (state-variable), title and description for the new-todos (that will get added)
+
+  // When creating Dynamic websites using React we should always want less Components to get re-render (as component re-rendering is expensive)
+
+  // Implementing the above concepts so that there should be less number of Components that gets re-renders (again gets called to update the state)
+  const [title, setTitle] = useState(""); 
+  const [description, setDescription] = useState("");
+
+  const [allTodos, setAllTodos] = useState([{
+    title: "Hello Todo 1",
+    description: "Hello Todo 1"
+  }, {
+    title: "Hello Todo 2",
+    description: "Hello Todo 2"
+  }, {
+    title: "Hello Todo 3",
+    description: "Hello Todo 3"
+  }])
+
+  function addTodo() {
+    const finalTitle = title;
+    const finalDescription = description;
+
+    setTitle("");
+    setDescription("");
+
+    setAllTodos([...allTodos, {
+      title: finalTitle,
+      description: finalDescription
+    }])
+  }
+  
+  return (
+    <div className="todo-container" >
+      {/* this will re-render when title and description gets updated */}
+      <TodoInputMemo title={title} description={description} setTitle={setTitle} setDescription={setDescription} addTodo={addTodo} />
+      
+      {/* this will be only re-render when the allTodos gets updated using memo */}
+      <AllTodosMemo allTodos={allTodos} />
+    </div>
+  )
+}
+
+
+export { TodoApp };
+
+// Updation of state-variable leads to re-rendering of Parent + Child Components (if not using memo then all Children get re-render ) (if using memo with Children than only that Children will re-render whose state variable changes + Parent)
